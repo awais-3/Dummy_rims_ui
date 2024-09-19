9c93459621314309412b18ceb3582901e485924e
Awais Khan <awais@contrivers.dev>
2024-09-19 14:11:28 +0500

    Fixed Puntuation

--- src/app/Components/Common/DetailCard.js
+++ src/app/Components/Common/DetailCard.js
@@ -1,11 +1,19 @@
 import { Box, Text } from "@chakra-ui/react";
 
+const formatLabel = (label) => {
+  return label
+    .replace(/([a-z0-9])([A-Z])/g, "$1 $2") 
+    .replace(/_/g, " ") // Replace underscores with spaces (if any)
+    .toLowerCase() // Convert all to lowercase first
+    .replace(/\b(\w)/g, (char) => char.toUpperCase()); // Capitalize first letter of each word
+};
+
 const DetailCard = ({ label, value }) => (
   <Box display={"flex"} gap={2} mt={2} flexWrap={"wrap"}>
-    <Text fontWeight="600" color="gray.600" mb="1" textTransform={"uppercase"}>
-      {label}:
+    <Text fontWeight="700" color="black" mb="1">
+      {formatLabel(label)}:
     </Text>
-    <Text color="black" fontWeight={700}>
+    <Text color="gray.700" fontWeight={500}>
       {value}
     </Text>
   </Box>
